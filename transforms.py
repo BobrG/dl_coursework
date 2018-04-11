609f60231349f0d3d29f7875e829967d168652d4
Gleb Bobrovskikh <bobrovskihg@gmail.com>
2018-04-11 19:47:05 +0300

    added augmentations

---
+++ transforms.py
@@ -0,0 +1,165 @@
+import random
+import cv2
+import numpy as np
+import math
+
+'''
+    Implementation from here:
+        https://github.com/asanakoy/kaggle_carvana_segmentation/blob/master/albu/src/transforms.py
+'''
+
+class VerticalFlip:
+    def __init__(self, prob=0.5):
+        self.prob = prob
+
+    def __call__(self, img, mask=None):
+        if random.random() < self.prob:
+            img = cv2.flip(img, 0)
+            if mask is not None:
+                mask = cv2.flip(mask, 0)
+        return img, mask
+
+class HorizontalFlip:
+    def __init__(self, prob=0.5):
+        self.prob = prob
+
+    def __call__(self, img, mask=None):
+        if random.random() < self.prob:
+            img = cv2.flip(img, 1)
+            if mask is not None:
+                mask = cv2.flip(mask, 1)
+        return img, mask
+
+class RandomFlip:
+    def __init__(self, prob=0.5):
+        self.prob = prob
+
+    def __call__(self, img, mask=None):
+        if random.random() < self.prob:
+            d = random.randint(-1, 1)
+            img = cv2.flip(img, d)
+            if mask is not None:
+                mask = cv2.flip(mask, d)
+        return img, mask
+
+class Transpose:
+    def __init__(self, prob=0.5):
+        self.prob = prob
+
+    def __call__(self, img, mask=None):
+        if random.random() < self.prob:
+            img = img.transpose(1, 0, 2)
+            if mask is not None:
+                mask = mask.transpose(1, 0)
+        return img, mask
+
+class Rotate:
+    def __init__(self, limit=90, prob=0.5):
+        self.prob = prob
+        self.limit = limit
+
+    def __call__(self, img, mask=None):
+        if random.random() < self.prob:
+            angle = random.uniform(-self.limit, self.limit)
+
+            height, width = img.shape[0:2]
+            mat = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1.0)
+            img = cv2.warpAffine(img, mat, (height, width),
+                                 flags=cv2.INTER_LINEAR,
+                                 borderMode=cv2.BORDER_REFLECT_101)
+            if mask is not None:
+                mask = cv2.warpAffine(mask, mat, (height, width),
+                                      flags=cv2.INTER_LINEAR,
+                                      borderMode=cv2.BORDER_REFLECT_101)
+
+        return img, mask
+
+class RandomCrop:
+    def __init__(self, size):
+        self.h = size[0]
+        self.w = size[1]
+
+    def __call__(self, img, mask=None):
+        height, width, _ = img.shape
+
+        h_start = np.random.randint(0, height - self.h)
+        w_start = np.random.randint(0, width - self.w)
+
+        img = img[h_start: h_start + self.h, w_start: w_start + self.w]
+
+        assert img.shape[0] == self.h
+        assert img.shape[1] == self.w
+
+        if mask is not None:
+            mask = mask[h_start: h_start + self.h, w_start: w_start + self.w]
+
+        return img, mask
+
+class Shift:
+    def __init__(self, limit=4, prob=.5):
+        self.limit = limit
+        self.prob = prob
+
+    def __call__(self, img, mask=None):
+        if random.random() < self.prob:
+            limit = self.limit
+            dx = round(random.uniform(-limit, limit))
+            dy = round(random.uniform(-limit, limit))
+
+            height, width, channel = img.shape
+            y1 = limit + 1 + dy
+            y2 = y1 + height
+            x1 = limit + 1 + dx
+            x2 = x1 + width
+
+            img1 = cv2.copyMakeBorder(img, limit + 1, limit + 1, limit + 1, limit + 1,
+                                      borderType=cv2.BORDER_REFLECT_101)
+            img = img1[y1:y2, x1:x2, :]
+            if mask is not None:
+                msk1 = cv2.copyMakeBorder(mask, limit + 1, limit + 1, limit + 1, limit + 1,
+                                          borderType=cv2.BORDER_REFLECT_101)
+                mask = msk1[y1:y2, x1:x2, :]
+
+        return img, mask
+
+class CenterCrop:
+    def __init__(self, size):
+        self.height = size[0]
+        self.width = size[1]
+
+    def __call__(self, img, mask=None):
+        h, w, c = img.shape
+        dy = (h - self.height) // 2
+        dx = (w - self.width) // 2
+
+        y1 = dy
+        y2 = y1 + self.height
+        x1 = dx
+        x2 = x1 + self.width
+        img = img[y1:y2, x1:x2]
+
+        if mask is not None:
+            mask = mask[y1:y2, x1:x2]
+
+        return img, mask
+
+class RandomFilter:
+    '''
+        blur sharpen, etc
+    '''
+    def __init__(self, limit=.5, prob=.5):
+        self.limit = limit
+        self.prob = prob
+
+    def __call__(self, img):
+        if random.random() < self.prob:
+            alpha = self.limit * random.uniform(0, 1)
+            kernel = np.ones((3, 3), np.float32) / 9 * 0.2
+
+            colored = img[..., :3]
+            colored = alpha * cv2.filter2D(colored, -1, kernel) + (1 - alpha) * colored
+            maxval = np.max(img[..., :3])
+            dtype = img.dtype
+            img[..., :3] = clip(colored, dtype, maxval)
+
+        return img
