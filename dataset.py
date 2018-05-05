90cf2f72e551957f088497bf0d833a764ccf3069
Gleb Bobrovskikh <bobrovskihg@gmail.com>
2018-05-05 13:11:21 +0000

    added new dataset, added combine dataset functions

--- dataset.py
+++ dataset.py
@@ -1,16 +1,14 @@
-import numpy as np
 import os
-import scipy.io as sio
 import torch
-from torch.utils.data.dataset import Dataset
-from torchvision.transforms import ToTensor
+import numpy as np
+import scipy.io as sio
 from utils import load_image
-import cv2
-import PIL
+from torchvision.transforms import ToTensor
+from torch.utils.data.dataset import Dataset
 
 #SURREAL DATASET LOADER
 class SURREALDataset(Dataset):
-    def __init__(self, dirry, num_classes, transforms=None, identifier=None, lengt=None, weights=None, weight_type=None):
+    def __init__(self, dirry, num_classes, transforms=None, fixed_size=None, identifier=None, lengt=None, weights=None, weight_type=None):
         self.pics = []
         
         subdirs = [i[0] for i in os.walk(dirry)]
@@ -26,10 +24,14 @@ class SURREALDataset(Dataset):
         self.identifier = identifier 
         self.curr_pic = []
         self.curr_mask = []
+        self.fixed_size = fixed_size
 
     def __getitem__(self, i):
         # get image and add padding to shape (x_height // 32 == 0, x_width // 32 == 0)
-        x, pad = load_image(self.pics[i], pad=True)
+        if self.fixed_size is not None:
+            x, pad = load_image(self.pics[i], pad=True, fixed_size=self.fixed_size=)
+        else:    
+            x, pad = load_image(self.pics[i], pad=True)
         # save current batch of pictures for further demonstration
         if len(self.curr_pic) >= 4:
             self.curr_pic = []
@@ -78,7 +80,7 @@ class SURREALDataset(Dataset):
             x[i] /= x[i].std()
             
         
-        y = torch.FloatTensor(y)
+        y = torch.FloatTensor(y[1:])#drop background
        
         return x, y
     
@@ -99,21 +101,27 @@ class SURREALDataset(Dataset):
     
 #SITTING PEOPLE DATASET LOADER    
 class SittingDataset(Dataset):
-    def __init__(self, dirry, num_classes, transforms=None, identifier=None, lengt=None):
+    def __init__(self, dirry, num_classes, transforms=None, fixed_size=None, identifier=None, lengt=None):
         self.pics = []
-        for j in [k for k in os.listdir(dirry) if k.endswith('jpg')]:
+        subdirs = [k for k in os.listdir(dirry) if k.endswith('jpg')]
+        for j in subdirs:
             self.pics.append(dirry + '/' + j)
+        
 
         self.classes = num_classes
         self.transforms = transforms
         self.identifier = identifier 
         self.curr_pic = []
         self.curr_mask = []
-    
+        self.fixed_size = fixed_size
+
     def __getitem__(self, i):
         # get image 
-        x, pad = load_image(self.pics[i], pad=True)
-        
+        if self.fixed_size is not None:
+            x, pad = load_image(self.pics[i], pad=True, fixed_size=self.fixed_size=)
+        else:    
+            x, pad = load_image(self.pics[i], pad=True)
+       
         if len(self.curr_pic) >= 4:
             self.curr_pic = []
             self.curr_mask = []
@@ -125,7 +133,7 @@ class SittingDataset(Dataset):
         mask = sio.loadmat(tmp[0] + 'masks' + tmp[-1][:-4] + '.mat')['M']   
         y = np.zeros((self.classes, x.shape[0], x.shape[1]))
         
-        
+        # change segmentation maps
         if self.identifier == 'restructed':
             
             mask[(mask == 5) + (mask == 8)] = 8
@@ -138,12 +146,12 @@ class SittingDataset(Dataset):
             mask[(mask == 11) + (mask == 14)] = 4
         
 
-         # transorm ToTensor + add normalization to zero mean and unit std
+        # transorm ToTensor + add normalization to zero mean and unit std
         if self.transforms is not None:
             x, mask = self.transforms(x, mask)
 
         self.curr_mask.append(mask)   
-        x = torch.from_numpy(np.moveaxis(x, -1, 0)).float()
+        #x = torch.from_numpy(np.moveaxis(x, -1, 0)).float()
         #binarizing mask
         for i in range(len(mask)):
             row = mask[i]
@@ -156,7 +164,7 @@ class SittingDataset(Dataset):
             
             x[i] /= x[i].std()
     
-            y = torch.FloatTensor(y) 
+        y = torch.FloatTensor(y[1:])#drop background
     
         return x, y
     
@@ -171,7 +179,82 @@ class SittingDataset(Dataset):
     
     def get_curr_mask(self):
         return self.curr_mask
-    
+
+#PASCAL PARTS DATASET LOADER
+class PascalDataset(Dataset):
+    def __init__(self, dirry, num_classes, transforms=None, fixed_size=None, identifier=None, lengt=None):
+        self.pics = []
+        self.masks = []
+        subdirs = [k for k in os.listdir(dirry) if k.endswith('mat')]
+        self.path_2_pic = '~/home/novikov/Dropbox/skoltech/students/Gleb/VOCdevkit/VOC2010/JPEGImages/'
+        self.path_2_mask = dirry
+        for j in subdirs:
+            self.pics.append(self.path_2_pic + '/' + j[:-4] + 'jpg')
+            self.masks.append(self.path_2_pic + '/' + j)
+        self.classes = num_classes
+        self.transforms = transforms
+        self.identifier = identifier 
+        self.curr_pic = []
+        self.curr_mask = []
+        # classes for futher reconstruction
+        self.classes_list = [
+            ['hair', 'head', 'lear', 'leye', 'lebrow', 'rear', 'reye', 'rebrow', 'nose'],
+            ['torso', 'neck'],
+            ['lhand', 'rhand'],
+            ['ruarm', 'luarm'],
+            ['rlarm', 'llarm'],
+            ['rlleg', 'llleg'],
+            ['ruleg', 'luleg'],
+            ['rfoot', 'lfoot']
+            ]
+        self.fixed_size = fixed_size
+
+    def  __getitem__(self, i):
+        if self.fixed_size is not None:
+            x, pad = load_image(self.pics[i], pad=True, fixed_size=self.fixed_size=)
+        else:    
+            x, pad = load_image(self.pics[i], pad=True)
+       
+        if len(self.curr_pic) >= 4:
+            self.curr_pic = []
+            self.curr_mask = []
+            
+        self.curr_pic.append(self.pics[i])
+        
+        # get segmentation map
+        y = np.zeros((self.classes, x.shape[0], x.shape[1]))
+        for i in tmp['anno'][0][0][1][0]:
+            if (i[0][0] == 'person'):
+                #background
+                #y[0] = cv2.copyMakeBorder(-1*(i[2][0] - np.ones(i[2][0].shape)), pad, cv2.BORDER_REFLECT_101) 
+                for j, mask in enumerate(i[3][0]):
+                    for c, class_ in enumerate(self.classes_list):
+                        if mask[0][0] in class_:
+                            y[c] = cv2.copyMakeBorder(mask[1], pad, cv2.BORDER_REFLECT_101)
+                break
+
+        if self.transforms is not None:
+            x, y = self.transforms(x, y)
+        y = torch.FloatTensor(y)
+
+        return x, y
+
+#Combination Dataset
+class CombineDataset(Dataset):
+    def __init__(self, datasets):
+        self.datasets = datasets
+        self.lengts = [len(i) for i in datasets]
+        self.offsets = np.cumsum(self.lengths)
+        self.length = np.sum(self.lengths)
+    def __getitem__(self, index):
+        for i, offset in enumerate(self.offsets):
+            if (index < offset and i > 0):
+                index -= self.offsets[i-1]
+                return self.datasets[i][index]
+    def __len__(self):
+        return self.length
+
+#TEMPORARY Sur + Real dataset
 class Sur_and_Real(Dataset):
     def __init__(self, dirry, num_classes, transforms=None, identifier=None, lengt=None, ind_1=None, ind_2=None):
         self.pics = []
@@ -185,9 +268,11 @@ class Sur_and_Real(Dataset):
 
         for i in subdirs:
             for j in [k for k in os.listdir(i) if k.endswith('jpg')]:
-                self.pics.append(i + '/' + j)
+                
                 if (len(self.pics) > lengt and lengt is not None):
                     break
+                else:
+                    self.pics.append(i + '/' + j)
 
         self.classes = num_classes
         self.transforms = transforms
@@ -199,6 +284,7 @@ class Sur_and_Real(Dataset):
                
         # get image and add padding to shape (x_height // 32 == 0, x_width // 32 == 0)
         x, pad = load_image(self.pics[i], pad=True)
+        #x = cv2.copyMakeBorder(plt.imread(self.pics[i]), 10, y_max_pad, x_min_pad, x_max_pad, cv2.BORDER_REFLECT_101)
 
         # save current batch of pictures for further demonstration
         if len(self.curr_pic) >= 4:
@@ -212,8 +298,6 @@ class Sur_and_Real(Dataset):
         if 'sur' in self.pics[i]: 
             tmp = self.pics[i].split('frame')
             mask = sio.loadmat(tmp[0] + '_segm.mat')['segm_' + str(int(tmp[1][0:-4]) + 1)] 
-            
-            y = np.zeros((self.classes, x.shape[0], x.shape[1]))
             
             if self.identifier == 'restructed':
                 mask[(mask == 2) + (mask == 3)] = 1
@@ -248,7 +332,7 @@ class Sur_and_Real(Dataset):
             x, mask = self.transforms(x, mask)
 
         self.curr_mask.append(mask)   
-        x = torch.from_numpy(np.moveaxis(x, -1, 0)).float()
+        #x = torch.from_numpy(np.moveaxis(x, -1, 0)).float()
 
         #binarizing mask
         for i in range(len(mask)):
@@ -263,8 +347,8 @@ class Sur_and_Real(Dataset):
             x[i] /= x[i].std()
             
         
-        y = torch.FloatTensor(y)
-       
+        y = torch.FloatTensor(y)#drop background
+        
         return x, y
     
     def __len__(self):
